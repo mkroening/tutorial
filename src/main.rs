6133976e70739a08cd88ed1f59c8bb61181ab006
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2022-06-28 06:02:29 +0200

    extend the list of content types

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,12 @@ fn get_content_type(filename: &str) -> &'static str {
 		"jpeg" => "image/jpeg",
 		"png" => "image/png",
 		"pdf" => "application/pdf",
+		"ttf" => "font/ttf",
+		"woff" => "font/woff",
+		"woff2" => "font/woff2",
+		"js" => "text/javascript",
+		"csv" => "text/csv; charset=utf8",
+		"css" => "text/css; charset=utf8",
 		"htm" => "text/html; charset=utf8",
 		"html" => "text/html; charset=utf8",
 		"txt" => "text/plain; charset=utf8",
@@ -43,20 +49,37 @@ fn main() {
 			request.url()
 		};
 
+		let content_type = get_content_type(filename);
+
 		match fs.open_file(filename) {
 			Ok(mut file) => {
-				let mut text = String::new();
-				file.read_to_string(&mut text).unwrap();
+				let response = match content_type {
+					"text/plain"
+					| "text/plain; charset=utf8"
+					| "text/html; charset=utf8"
+					| "text/csv; charset=utf8"
+					| "text/txt; charset=utf8" => {
+						let mut text = String::new();
+						file.read_to_string(&mut text).unwrap();
 
-				let response = tiny_http::Response::from_string(text);
+						tiny_http::Response::from_string(text)
+					}
+					_ => {
+						let mut data: Vec<u8> = Vec::new();
+						file.read_to_end(&mut data).unwrap();
+
+						tiny_http::Response::from_data(data)
+					}
+				};
+
 				let response = response.with_header(tiny_http::Header {
 					field: "Content-Type".parse().unwrap(),
-					value: AsciiString::from_ascii(get_content_type(filename)).unwrap(),
+					value: AsciiString::from_ascii(content_type).unwrap(),
 				});
-
 				let _ = request.respond(response);
 			}
 			Err(_) => {
+				println!("Unable to find {}", request.url());
 				let response = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
 				let _ = request.respond(response);
 			}
